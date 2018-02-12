16026176de2697b0f335563eb54308e1694330b6
Samuel Ainsworth <skainsworth@gmail.com>
2018-02-11 22:08:07 -0800

    add module and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+import babar
+
+
+setup(
+  name='babar',
+  version=babar.__version__,
+  description='Elephants never forget.',
+  author='Samuel Ainsworth',
+  author_email='skainsworth@gmail.com',
+  url='https://github.com/samuela/babar',
+  packages=['babar'],
+  install_requires=[]
+)
